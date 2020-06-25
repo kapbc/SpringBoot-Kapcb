51a5e1ac363c2548e1c0588cf09650622da71371
kapccc <eircccallroot@163.com>
2020-06-25 20:16:30 +0800

    SpringBoot+MyBatis+Cache+Redis

---
+++ springboot-kapcb-redis/src/main/java/com/kapcb/ccc/SpringbootKapcbRedisApplication.java
@@ -0,0 +1,13 @@
+package com.kapcb.ccc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootKapcbRedisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootKapcbRedisApplication.class, args);
+    }
+
+}
