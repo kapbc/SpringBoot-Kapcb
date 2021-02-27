d3732ad7c1f14f9e7fa72b84198d1e50894d12a0
Eircc <2530628867@qq.com>
2021-02-27 12:27:44 +0800

    SpringBoot-AMQP-Rabbit-Producer

--- SpringBoot-AMQP-Rabbit-Producer/src/main/java/com/kapcb/ccc/commons/config/RabbitConfiguration.java
+++ SpringBoot-AMQP-Rabbit-Producer/src/main/java/com/kapcb/ccc/commons/config/RabbitConfiguration.java
@@ -1,5 +1,17 @@
 package com.kapcb.ccc.commons.config;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.connection.CorrelationData;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+import org.springframework.util.concurrent.SettableListenableFuture;
+
 /**
  * <a>Title: RabbitConfiguration </a>
  * <a>Author: kapcb <a>
@@ -9,5 +21,69 @@ package com.kapcb.ccc.commons.config;
  * @version 1.0.0
  * @date 2021/2/27 11:54
  */
+@Slf4j
+@Configuration
 public class RabbitConfiguration {
+
+    @Bean
+    public MessageConverter messageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+
+    /**
+     * 生产者推送消息的消息确认调用回调函数已经完毕
+     * <p>
+     * 推送消息存在四种情况：
+     * ①消息推送到server，但是在server里找不到交换机
+     * ②消息推送到server，找到交换机了，但是没找到队列
+     * ③消息推送到sever，交换机和队列啥都没找到
+     * ④消息推送成功
+     *
+     * @param connectionFactory ConnectionFactory
+     * @return RabbitTemplate
+     */
+    @Bean(value = "rabbitTemplate")
+    @Scope(value = "pro")
+    public RabbitTemplate createRabbitTemplate(ConnectionFactory connectionFactory) {
+        RabbitTemplate rabbitTemplate = new RabbitTemplate();
+        rabbitTemplate.setConnectionFactory(connectionFactory);
+        /**
+         * 设置开启Mandatory才能触发回调函数，无论推送消息结果如何都强制调用回调函数
+         */
+        rabbitTemplate.setMandatory(true);
+        rabbitTemplate.setConfirmCallback((correlationData, ack, cause) -> {
+            log.info("RabbitConfirmCallback        correlationData : " + correlationData);
+            log.info("RabbitConfirmCallback        ack : " + ack);
+            log.info("RabbitConfirmCallback        cause : " + cause);
+
+            if (ack) {
+                if (correlationData!=null){
+                    String id = correlationData.getId();
+                    SettableListenableFuture<CorrelationData.Confirm> future = correlationData.getFuture();
+                    Message returnedMessage = correlationData.getReturnedMessage();
+                    Class<? extends CorrelationData> aClass = correlationData.getClass();
+                    log.info("the correlationData's id is : " + id);
+                    log.info("the correlationData's future is : " + future);
+                    log.info("the correlationData's returnedMessage is : " + returnedMessage);
+                    log.info("the correlationData's aClass is : " + aClass);
+                }
+            } else {
+                /**
+                 * 失败进行具体的后续处理：重试 或者补偿等手段
+                 */
+                log.info("send message to rabbitmq exchange error!");
+            }
+        });
+
+        rabbitTemplate.setReturnCallback((message, replyCode, replyText, exchange, routingKey) -> {
+            log.info("RabbitReturnCallback        message : " + message);
+            log.info("RabbitReturnCallback        replyCode : " + replyCode);
+            log.info("RabbitReturnCallback        replyText : " + replyText);
+            log.info("RabbitReturnCallback        exchange : " + exchange);
+            log.info("RabbitReturnCallback        routingKey : " + routingKey);
+        });
+
+        rabbitTemplate.setMessageConverter(messageConverter());
+        return rabbitTemplate;
+    }
 }
---
+++ SpringBoot-AMQP-Rabbit-Producer/src/main/java/com/kapcb/ccc/commons/config/exchange/RabbitDirectExchange.java
@@ -0,0 +1,59 @@
+package com.kapcb.ccc.commons.config.exchange;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * <a>Title: DirectExchange </a>
+ * <a>Author: kapcb <a>
+ * <a>Description: <a>
+ *
+ * @author kapcb
+ * @version 1.0.0
+ * @date 2021/2/27 11:54
+ */
+@Slf4j
+@Configuration
+public class RabbitDirectExchange {
+
+    @Bean(value = "directExchangeOne")
+    public org.springframework.amqp.core.DirectExchange directExchangeOne() {
+        log.info("begin to create rabbit exchange bean of directExchangeOne...");
+        return new org.springframework.amqp.core.DirectExchange("directExchangeOne", true, false);
+    }
+
+    @Bean(value = "directExchangeTwo")
+    public org.springframework.amqp.core.DirectExchange directExchangeTwo() {
+        log.info("begin to create rabbit exchange bean of directExchangeTwo...");
+        return new org.springframework.amqp.core.DirectExchange("directExchangeTwo", true, false);
+    }
+
+    @Bean(value = "directQueueOne")
+    public Queue directQueueOne() {
+        log.info("begin to create rabbit queue bean of directQueueOne...");
+        return new Queue("directQueueOne", true, false, false);
+    }
+
+    @Bean(value = "directQueueTwo")
+    public Queue directQueueTwo() {
+        log.info("begin to create rabbit queue bean of directQueueTwo...");
+        return new Queue("directQueueTwo", true, false, false);
+    }
+
+    @Bean
+    public Binding directQueueBingingOne(@Qualifier(value = "directQueueOne") Queue directQueueOne, @Qualifier(value = "directExchangeOne") org.springframework.amqp.core.DirectExchange directExchangeOne) {
+        log.info("begin to binding directQueueOne to  directExchangeOne...");
+        return BindingBuilder.bind(directQueueOne).to(directExchangeOne).with("rabbit.direct.one");
+    }
+
+    @Bean
+    public Binding directQueueBindingTwo(@Qualifier(value = "directQueueTwo") Queue directQueueTwo, @Qualifier(value = "directExchangeTwo") org.springframework.amqp.core.DirectExchange directExchangeTwo) {
+        log.info("begin to binding directQueueTwo to directExchangeTwo...");
+        return BindingBuilder.bind(directQueueTwo).to(directExchangeTwo).with("rabbit.direct.two");
+    }
+}
