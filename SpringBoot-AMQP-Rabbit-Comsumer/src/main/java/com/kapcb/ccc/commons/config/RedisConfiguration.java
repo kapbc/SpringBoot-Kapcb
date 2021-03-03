f524a6555b7aeae96fe4ee404c3c3e1969e5fd23
kapcb <eircccallroot@yeah.net>
2021-03-03 21:42:03 +0800

    SpringBoot-AMQP-Rabbit-Consumer

---
+++ SpringBoot-AMQP-Rabbit-Comsumer/src/main/java/com/kapcb/ccc/commons/config/RedisConfiguration.java
@@ -0,0 +1,21 @@
+package com.kapcb.ccc.commons.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * <a>Title: RedisConfiguration </a>
+ * <a>Author: kapcb <a>
+ * <a>Description: <a>
+ *
+ * @author kapcb
+ * @version 1.0.0
+ * @date 2021/3/3 21:40
+ */
+@Slf4j
+@Configuration
+public class RedisConfiguration {
+
+
+}
