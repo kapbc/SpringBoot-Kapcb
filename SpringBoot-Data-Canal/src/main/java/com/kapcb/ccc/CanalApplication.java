454e05a9d1e32532eb8afc09148746f4bacfbe69
kapcb <eircccallroot@yeah.net>
2021-03-06 09:36:13 +0800

    SpringBoot-Data-CANAL

---
+++ SpringBoot-Data-Canal/src/main/java/com/kapcb/ccc/CanalApplication.java
@@ -0,0 +1,13 @@
+package com.kapcb.ccc;
+
+/**
+ * <a>Title: CanalApplication </a>
+ * <a>Author: kapcb <a>
+ * <a>Description: <a>
+ *
+ * @author kapcb
+ * @version 1.0.0
+ * @date 2021/3/6 9:24
+ */
+public class CanalApplication {
+}
---
+++ SpringBoot-Data-Canal/src/main/java/com/kapcb/ccc/commons/config/CanalConfiguration.java
@@ -0,0 +1,13 @@
+package com.kapcb.ccc.commons.config;
+
+/**
+ * <a>Title: CanalConfiguration </a>
+ * <a>Author: kapcb <a>
+ * <a>Description: <a>
+ *
+ * @author kapcb
+ * @version 1.0.0
+ * @date 2021/3/6 9:35
+ */
+public class CanalConfiguration {
+}
