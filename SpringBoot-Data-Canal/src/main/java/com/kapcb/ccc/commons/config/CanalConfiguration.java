5886a7113eaa96d798318fda1f2da755139e3051
kapcb <eircccallroot@yeah.net>
2021-03-06 09:37:53 +0800

    SpringBoot-Data-Canal

--- SpringBoot-Data-Canal/src/main/java/com/kapcb/ccc/commons/config/CanalConfiguration.java
+++ SpringBoot-Data-Canal/src/main/java/com/kapcb/ccc/commons/config/CanalConfiguration.java
@@ -1,5 +1,8 @@
 package com.kapcb.ccc.commons.config;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+
 /**
  * <a>Title: CanalConfiguration </a>
  * <a>Author: kapcb <a>
@@ -9,5 +12,7 @@ package com.kapcb.ccc.commons.config;
  * @version 1.0.0
  * @date 2021/3/6 9:35
  */
+@Slf4j
+@Configuration
 public class CanalConfiguration {
 }
