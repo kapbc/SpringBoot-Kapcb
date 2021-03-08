cde1b846a3804da3253787f829a154e9c45529a1
kapcb <55942617+kapbc@users.noreply.github.com>
2021-03-08 18:07:06 +0800

    Create Swagger2Config.java

---
+++ SpringBoot-Data-Canal/src/main/java/com/kapcb/ccc/commons/config/Swagger2Config.java
@@ -0,0 +1,35 @@
+/**
+ * <a>Title: Swagger2Config </a>
+ * <a>Author: Mike Chen <a>
+ * <a>Description：<a>
+ *
+ * @author Mike Chen
+ * @date 2021/3/4-10:02
+ */
+@Configuration
+@EnableSwagger2
+public class Swagger2Config {
+
+    @Bean
+    public Docket getOrderDocket(){
+        ApiInfo apiInfo=new ApiInfoBuilder()
+                //api标题
+                .title("订单管理")
+                //api描述
+                .description("用户管理相关接口描述")
+                //版本号
+                .version("1.0.0")
+                .build();
+        //文档类型（swagger2）
+        return new Docket(DocumentationType.SWAGGER_2)
+                //设置包含在json ResourceListing响应中的api元信息
+                .apiInfo(apiInfo)
+                //启动用于api选择的构建器
+                .select()
+                //扫描接口的包
+                .apis(RequestHandlerSelectors.basePackage("com.kapcb.ccc.controller"))
+                //路径过滤器（扫描所有路径）
+                .paths(PathSelectors.any())
+                .build();
+    }
+}
