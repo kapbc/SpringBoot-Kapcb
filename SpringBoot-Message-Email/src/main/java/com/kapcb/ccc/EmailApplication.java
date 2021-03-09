920c7b3599afeeabfbbc2aa2505e75149241a7c2
kapcb <eircccallroot@yeah.net>
2021-03-09 23:04:12 +0800

    SpringBoot-Message-Email

---
+++ SpringBoot-Message-Email/src/main/java/com/kapcb/ccc/EmailApplication.java
@@ -0,0 +1,17 @@
+package com.kapcb.ccc;
+
+/**
+ * <a>Title: EmailApplication </a>
+ * <a>Author: kapcb <a>
+ * <a>Description: <a>
+ *
+ * @author kapcb
+ * @version 1.0.0
+ * @date 2021/3/9 23:03
+ */
+public class EmailApplication {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ SpringBoot-Message-Email/src/main/java/com/kapcb/ccc/SystemController.java
@@ -0,0 +1,13 @@
+package com.kapcb.ccc;
+
+/**
+ * <a>Title: SystemController </a>
+ * <a>Author: kapcb <a>
+ * <a>Description: <a>
+ *
+ * @author kapcb
+ * @version 1.0.0
+ * @date 2021/3/9 23:02
+ */
+public class SystemController {
+}
