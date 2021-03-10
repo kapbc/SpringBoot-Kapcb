8a270ac4a46a7abbf281a9074d426c01e942d504
kapcb <eircccallroot@yeah.net>
2021-03-10 22:17:36 +0800

    SpringBoot-Generator-Template

---
+++ SpringBoot-Generator-Template/src/main/java/com/kapcb/ccc/Controller.java
@@ -0,0 +1,13 @@
+package com.kapcb.ccc;
+
+/**
+ * <a>Title: Controller </a>
+ * <a>Author: kapcb <a>
+ * <a>Description: <a>
+ *
+ * @author kapcb
+ * @version 1.0.0
+ * @date 2021/3/10 22:15
+ */
+public class Controller {
+}
