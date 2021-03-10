911f0a913df3a76c42d9b7e0da37225543f86e90
kapcb <55942617+kapbc@users.noreply.github.com>
2021-03-10 08:50:45 +0800

    Create EmailApplication.java

---
+++ SpringBoot-Message-Email/src/main/java/com/kapcb/ccc/EmailApplication.java
@@ -0,0 +1,19 @@
+package com.kapcb.ccc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * <a>Title: EmailApplication </a>
+ * <a>Author: Mike Chen <a>
+ * <a>Description：<a>
+ *
+ * @author Mike Chen
+ * @date 2021/3/8-18:51
+ */
+@SpringBootApplication
+public class EmailApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EmailApplication.class, args);
+    }
+}
