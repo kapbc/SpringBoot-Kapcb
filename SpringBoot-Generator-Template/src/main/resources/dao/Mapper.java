989b36e6fcf7539a8c033f4d277681ca60d673b1
kapcb <55942617+kapbc@users.noreply.github.com>
2021-03-11 09:09:23 +0800

    Create Mapper.java

---
+++ SpringBoot-Generator-Template/src/main/resources/dao/Mapper.java
@@ -0,0 +1,14 @@
+package ${package_mapper};
+
+import ${package_pojo}.${Table};
+import tk.mybatis.mapper.common.Mapper;
+
+/**
+ * <a>Author: Mike Chen <a>
+ * <a>Description：<a>
+ *
+ * @author Mike Chen
+ * @date 2021/3/3-17:25
+ */
+public interface ${Table}Mapper extends Mapper<${Table}> {
+}
