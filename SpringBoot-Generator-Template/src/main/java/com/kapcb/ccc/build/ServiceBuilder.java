68c93ff91909cc7eadb77b1e4fa6822e3a33f589
kapcb <55942617+kapbc@users.noreply.github.com>
2021-03-11 09:03:51 +0800

    Create ServiceBuilder.java

---
+++ SpringBoot-Generator-Template/src/main/java/com/kapcb/ccc/build/ServiceBuilder.java
@@ -0,0 +1,28 @@
+package com.kapcb.ccc.build;
+
+import java.util.Map;
+
+/**
+ * <a>Author: Mike Chen <a>
+ * <a>Description：<a>
+ *
+ * @author Mike Chen
+ * @date 2021/3/3-17:25
+ */
+public class ServiceBuilder {
+
+
+    /***
+     * 构建Service
+     * @param modelMap
+     */
+    public static void builder(Map<String,Object> modelMap){
+        //生成Service层文件
+        BuilderFactory.builder(modelMap,
+                "/template/service",
+                "Service.java",
+                TemplateBuilder.PACKAGE_SERVICE_INTERFACE,
+                "Service.java");
+    }
+
+}
