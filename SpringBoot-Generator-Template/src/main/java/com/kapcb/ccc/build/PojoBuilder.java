468da6eb12baac3b5ac5c249d48d77f6af244e19
kapcb <55942617+kapbc@users.noreply.github.com>
2021-03-11 09:03:39 +0800

    Create PojoBuilder.java

---
+++ SpringBoot-Generator-Template/src/main/java/com/kapcb/ccc/build/PojoBuilder.java
@@ -0,0 +1,28 @@
+package com.kapcb.ccc.build;
+
+import java.util.Map;
+
+/**
+ * <a>Author: Mike Chen <a>
+ * <a>Description：<a>
+ *
+ * @author Mike Chen
+ * @date 2021/3/3-17:25
+ */
+public class PojoBuilder {
+
+
+    /***
+     * 构建Pojo
+     * @param dataModel
+     */
+    public static void builder(Map<String,Object> dataModel){
+        //生成Pojo层文件
+        BuilderFactory.builder(dataModel,
+                "/template/pojo",
+                "Pojo.java",
+                TemplateBuilder.PACKAGE_POJO,
+                ".java");
+    }
+
+}
