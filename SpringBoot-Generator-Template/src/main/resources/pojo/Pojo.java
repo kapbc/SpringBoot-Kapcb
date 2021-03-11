1f3fd66e0341373e6290e007f4c584cb9a3aa4ba
kapcb <55942617+kapbc@users.noreply.github.com>
2021-03-11 09:09:55 +0800

    Create Pojo.java

---
+++ SpringBoot-Generator-Template/src/main/resources/pojo/Pojo.java
@@ -0,0 +1,55 @@
+package ${package_pojo};
+
+<#if swagger==true>
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+</#if>
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.io.Serializable;
+<#list typeSet as set>
+import ${set};
+</#list>
+
+/**
+ * <a>Author: Mike Chen <a>
+ * <a>Description：<a>
+ *
+ * @author Mike Chen
+ * @date 2021/3/3-17:25
+ */
+<#if swagger==true>
+@ApiModel(description = "${Table}",value = "${Table}")
+</#if>
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(callSuper = false)
+@Table(name="${TableName}")
+public class ${Table} implements Serializable{
+
+<#list models as model>
+	/**
+	 * ${model.desc!""}
+	 */
+	<#if swagger==true>
+	@ApiModelProperty(value = "${model.desc!""}",required = false)
+	</#if>
+	<#if model.id==true>
+	@Id
+	<#if model.identity=='YES'>
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+	</#if>
+	</#if>
+    @Column(name = "${model.column}")
+	private ${model.simpleType} ${model.name};
+
+</#list>
+}
