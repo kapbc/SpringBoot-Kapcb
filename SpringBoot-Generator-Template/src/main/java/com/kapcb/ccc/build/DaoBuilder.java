64172364aed46951100720ed81c3f27ee462dbd3
kapcb <55942617+kapbc@users.noreply.github.com>
2021-03-11 09:03:18 +0800

    Create DaoBuilder.java

---
+++ SpringBoot-Generator-Template/src/main/java/com/kapcb/ccc/build/DaoBuilder.java
@@ -0,0 +1,28 @@
+package com.kapcb.ccc.build;
+
+import java.util.Map;
+
+/**
+ * <a>Author: Mike Chen <a>
+ * <a>Description：<a>
+ *
+ * @author Mike Chen
+ * @date 2021/3/3-17:25
+ */
+public class DaoBuilder {
+
+
+    /***
+     * 构建Dao
+     * @param modelMap
+     */
+    public static void builder(Map<String,Object> modelMap){
+        //生成Dao层文件
+        BuilderFactory.builder(modelMap,
+                "/template/dao",
+                "Mapper.java",
+                TemplateBuilder.PACKAGE_MAPPER,
+                "Mapper.java");
+    }
+
+}
