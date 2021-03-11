9e6dfaf1d1912ff8bda721d6844c1820a53f1ea3
kapcb <55942617+kapbc@users.noreply.github.com>
2021-03-11 09:06:57 +0800

    Create CodeApplication.java

---
+++ SpringBoot-Generator-Template/src/main/java/com/kapcb/CodeApplication.java
@@ -0,0 +1,20 @@
+package com.kapcb;
+
+import com.kapcb.ccc.build.TemplateBuilder;
+
+/**
+ * <a>Title: CodeApplication </a>
+ * <a>Author: Mike Chen <a>
+ * <a>Description：<a>
+ *
+ * @author Mike Chen
+ * @date 2021/3/10-19:24
+ */
+public class CodeApplication {
+
+    public static void main(String[] args) {
+        //调用该方法即可
+        TemplateBuilder.builder();
+    }
+
+}
