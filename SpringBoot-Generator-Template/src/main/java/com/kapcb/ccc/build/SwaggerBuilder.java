fdf2b2e00fcefcff8ef4e52e034344bba9528bb5
kapcb <55942617+kapbc@users.noreply.github.com>
2021-03-11 09:04:17 +0800

    Create SwaggerBuilder.java

---
+++ SpringBoot-Generator-Template/src/main/java/com/kapcb/ccc/build/SwaggerBuilder.java
@@ -0,0 +1,29 @@
+package com.kapcb.ccc.build;
+
+import java.util.Map;
+
+/**
+ * <a>Author: Mike Chen <a>
+ * <a>Description：<a>
+ *
+ * @author Mike Chen
+ * @date 2021/3/3-17:25
+ */
+public class SwaggerBuilder {
+
+    /***
+     * ServiceImpl构建
+     * @param modelMap
+     */
+    public static void builder(Map<String,Object> modelMap){
+        //swagger的文件名字
+        modelMap.put("Table","swagger");
+
+        //生成ServiceImpl层文件
+        BuilderFactory.builder(modelMap,
+                "/template/swagger",
+                "swagger.json",
+                TemplateBuilder.SWAGGERUI_PATH,
+                ".json");
+    }
+}
