9b677097b8053884bdbd9fd4e07beb87078fbb83
kapcb <55942617+kapbc@users.noreply.github.com>
2021-03-11 09:06:00 +0800

    Create SwaggerResponse.java

---
+++ SpringBoot-Generator-Template/src/main/java/com/kapcb/ccc/swagger/SwaggerResponse.java
@@ -0,0 +1,43 @@
+package com.kapcb.ccc.swagger;
+
+/**
+ * <a>Author: Mike Chen <a>
+ * <a>Description：<a>
+ *
+ * @author Mike Chen
+ * @date 2021/3/3-17:25
+ */
+public class SwaggerResponse {
+    //响应状态码  200,404.。。
+    private int code;
+
+    //描述
+    private String description;
+
+    //响应引用
+    private String schema;
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getSchema() {
+        return schema;
+    }
+
+    public void setSchema(String schema) {
+        this.schema = schema;
+    }
+}
