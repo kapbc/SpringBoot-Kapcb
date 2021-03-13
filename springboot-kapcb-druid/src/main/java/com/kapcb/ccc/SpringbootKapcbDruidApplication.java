37b97e1c22f5e547dd848612eea3c9609ebb35dc
kapcb <55942617+kapbc@users.noreply.github.com>
2021-03-13 23:49:04 +0800

    Update SpringbootKapcbDruidApplication.java

--- springboot-kapcb-druid/src/main/java/com/kapcb/ccc/SpringbootKapcbDruidApplication.java
+++ springboot-kapcb-druid/src/main/java/com/kapcb/ccc/SpringbootKapcbDruidApplication.java
@@ -7,9 +7,7 @@ import org.springframework.boot.web.servlet.ServletComponentScan;
 @SpringBootApplication
 //@ServletComponentScan//druid监控页面是一个servlet，需要让SpingBoot支持servlet
 public class SpringbootKapcbDruidApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(SpringbootKapcbDruidApplication.class, args);
     }
-
 }
