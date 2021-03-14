a65a9ddfbd2db8c4a2cc2f99becc60f2d3749a91
kapcb <eircccallroot@yeah.net>
2021-03-14 14:52:28 +0800

    SpringBoot-Message-Email

--- SpringBoot-Params-Validation/src/main/java/com/kapcb/ccc/ValidationApplication.java
+++ SpringBoot-Params-Validation/src/main/java/com/kapcb/ccc/ValidationApplication.java
@@ -1,5 +1,9 @@
 package com.kapcb.ccc;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
 /**
  * <a>Title: ValidationApplication </a>
  * <a>Author: kapcb <a>
@@ -9,5 +13,9 @@ package com.kapcb.ccc;
  * @version 1.0.0
  * @date 2021/3/14 14:21
  */
+@SpringBootApplication
 public class ValidationApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ValidationApplication.class, args);
+    }
 }
