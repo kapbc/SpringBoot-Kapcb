6fcd423ca93fdace867180c724f2c37ff757c949
kapcb <eircccallroot@yeah.net>
2021-03-14 14:25:02 +0800

    SpringBoot-params-Validator

---
+++ SpringBoot-Params-Validation/src/main/java/com/kapcb/ccc/ValidationApplication.java
@@ -0,0 +1,13 @@
+package com.kapcb.ccc;
+
+/**
+ * <a>Title: ValidationApplication </a>
+ * <a>Author: kapcb <a>
+ * <a>Description: <a>
+ *
+ * @author kapcb
+ * @version 1.0.0
+ * @date 2021/3/14 14:21
+ */
+public class ValidationApplication {
+}
---
+++ SpringBoot-Params-Validation/src/main/java/com/kapcb/ccc/commons/handler/GlobalExceptionHandler.java
@@ -0,0 +1,14 @@
+package com.kapcb.ccc.commons.handler;
+
+/**
+ * <a>Title: GlobalExceptionHandler </a>
+ * <a>Author: kapcb <a>
+ * <a>Description: <a>
+ *
+ * @author kapcb
+ * @version 1.0.0
+ * @date 2021/3/14 14:23
+ */
+
+public class GlobalExceptionHandler {
+}
