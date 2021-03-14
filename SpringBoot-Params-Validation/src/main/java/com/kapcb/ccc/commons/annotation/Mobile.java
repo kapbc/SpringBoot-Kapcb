0b34a07a58e2986e216644bc23707d69dd8caedb
kapcb <eircccallroot@yeah.net>
2021-03-14 21:05:24 +0800

    SpringBoot-Params-Validator

---
+++ SpringBoot-Params-Validation/src/main/java/com/kapcb/ccc/commons/annotation/Mobile.java
@@ -0,0 +1,33 @@
+package com.kapcb.ccc.commons.annotation;
+
+import com.kapcb.ccc.commons.validation.MobileValidator;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * <a>Title: Mobile </a>
+ * <a>Author: kapcb <a>
+ * <a>Description: <a>
+ *
+ * @author kapcb
+ * @version 1.0.0
+ * @date 2021/3/14 20:55
+ */
+@Documented
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+@Constraint(validatedBy = MobileValidator.class)
+public @interface Mobile {
+
+    String message();
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+}
--- SpringBoot-Params-Validation/src/main/java/com/kapcb/ccc/commons/validation/MobileValidator.java
+++ SpringBoot-Params-Validation/src/main/java/com/kapcb/ccc/commons/validation/MobileValidator.java
@@ -1,5 +1,14 @@
 package com.kapcb.ccc.commons.validation;
 
+import com.kapcb.ccc.commons.annotation.Mobile;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * <a>Title: MobileValidator </a>
  * <a>Author: kapcb <a>
@@ -9,5 +18,28 @@ package com.kapcb.ccc.commons.validation;
  * @version 1.0.0
  * @date 2021/3/14 14:27
  */
-public class MobileValidator {
+@Slf4j
+public class MobileValidator implements ConstraintValidator<Mobile, String> {
+
+    @Override
+    public void initialize(Mobile constraintAnnotation) {
+
+    }
+
+    @Override
+    public boolean isValid(String value, ConstraintValidatorContext context) {
+        try {
+            if (StringUtils.isBlank(value)) {
+                return true;
+            } else {
+                String mobileRegex = "[1]\\\\d{10}";
+                Pattern pattern = Pattern.compile(mobileRegex);
+                Matcher matcher = pattern.matcher(value);
+                return matcher.matches();
+            }
+        } catch (Exception e) {
+            log.error("the valid error message is : " + e.getMessage());
+            return false;
+        }
+    }
 }
