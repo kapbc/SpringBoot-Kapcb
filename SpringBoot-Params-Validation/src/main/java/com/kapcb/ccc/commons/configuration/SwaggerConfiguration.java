9af5d06f24f68e5d44ca5a813cc6cde42370594f
kapcb <eircccallroot@yeah.net>
2021-03-15 22:17:26 +0800

    SpringBoot-Params-Validator

--- SpringBoot-Params-Validation/src/main/java/com/kapcb/ccc/commons/configuration/SwaggerConfiguration.java
+++ SpringBoot-Params-Validation/src/main/java/com/kapcb/ccc/commons/configuration/SwaggerConfiguration.java
@@ -1,6 +1,14 @@
 package com.kapcb.ccc.commons.configuration;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
  * <a>Title: SwaggerConfiguration </a>
@@ -12,5 +20,23 @@ import org.springframework.context.annotation.Configuration;
  * @date 2021/3/14 14:31
  */
 @Configuration
+@EnableSwagger2
 public class SwaggerConfiguration {
+
+    @Bean
+    public Docket createRestfulApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .pathMapping("/")
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.kapcb.ccc.controller"))
+                .paths(PathSelectors.any())
+                .build().apiInfo(new ApiInfoBuilder()
+                        .title("SpringBoot-Params-Validator")
+                        .description("SpringBoot整合Swagger2详细信息....")
+                        .version("0.0.1")
+                        .contact(new Contact("kapcb", "www.kapcb.com", "eircccallroot@yeah.net"))
+                        .license("Apache License")
+                        .licenseUrl("www.apache.com")
+                        .build());
+    }
 }
