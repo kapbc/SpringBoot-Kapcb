ec6fd7b7414df4dd8017e2d98ceaaca2573b7698
kapcb <eircccallroot@yeah.net>
2021-03-16 22:11:49 +0800

    SpringBoot-Params-Validator

---
+++ SpringBoot-Data-TkMyBatis/src/main/java/com/kapcb/ccc/TkApplication.java
@@ -0,0 +1,21 @@
+package com.kapcb.ccc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * <a>Title: TkApplication </a>
+ * <a>Author: kapcb <a>
+ * <a>Description: <a>
+ *
+ * @author kapcb
+ * @version 1.0.0
+ * @date 2021/3/16 22:10
+ */
+@SpringBootApplication
+public class TkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TkApplication.class, args);
+    }
+}
