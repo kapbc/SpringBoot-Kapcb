fd6d04ca94d2a5b9f7004bb55914d4394f57077f
kapcb <eircccallroot@yeah.net>
2021-03-22 22:02:42 +0800

    SpringBoot-Data-MyBatis-Annotation

---
+++ SpringBoot-Data-MyBatis-Annotation/src/main/java/com/kapcb/ccc/MyBatisAnnotationApplication.java
@@ -0,0 +1,20 @@
+package com.kapcb.ccc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * <a>Title: MyBatisAnnotationApplication </a>
+ * <a>Author: kapcb <a>
+ * <a>Description: <a>
+ *
+ * @author kapcb
+ * @version 1.0.0
+ * @date 2021/3/22 22:00
+ */
+@SpringBootApplication
+public class MyBatisAnnotationApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MyBatisAnnotationApplication.class, args);
+    }
+}
