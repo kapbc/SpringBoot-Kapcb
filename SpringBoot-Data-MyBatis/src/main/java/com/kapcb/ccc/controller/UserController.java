6724f3424f92b90087e8d9db604fd3aefbe54366
kapcb <eircccallroot@yeah.net>
2021-03-27 23:40:31 +0800

    SpringBoot-Data-Params

--- SpringBoot-Data-MyBatis/src/main/java/com/kapcb/ccc/controller/UserController.java
+++ SpringBoot-Data-MyBatis/src/main/java/com/kapcb/ccc/controller/UserController.java
@@ -4,9 +4,12 @@ import com.github.pagehelper.PageInfo;
 import com.kapcb.ccc.pojo.UserPojo;
 import com.kapcb.ccc.service.UserService;
 import com.kapcb.ccc.service.impl.UserServiceImpl;
+import com.kapcb.ccc.vo.UserVO;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -31,17 +34,30 @@ public class UserController {
     private final UserService userService;
 
     @Autowired
-    public UserController(UserServiceImpl userService){
+    public UserController(UserServiceImpl userService) {
         this.userService = userService;
     }
 
     @GetMapping(value = "getUserList")
-    public Map<String, Object> getUserList(@RequestParam("page")Integer page, @RequestParam("size")Integer size){
-        log.info("the page is : "+page);
-        log.info("the size is : "+size);
+    public Map<String, Object> getUserList(@RequestParam("page") Integer page, @RequestParam("size") Integer size) {
+        log.info("the page is : " + page);
+        log.info("the size is : " + size);
         PageInfo<UserPojo> userVOList = userService.getUserVOList(page, size);
         Map<String, Object> resultMap = new HashMap<>(4);
-        resultMap.put("userList",userVOList);
+        resultMap.put("userList", userVOList);
+        return resultMap;
+    }
+
+    @GetMapping(value = "getUserById/{id}")
+    public Map<String, Object> getUserById(@PathVariable("id") Long id) {
+        log.info("the id is : " + id);
+        UserPojo user = userService.getUser(id);
+        UserVO userVO = new UserVO();
+        BeanUtils.copyProperties(user, userVO);
+        Map<String, Object> resultMap = new HashMap<>(4);
+        resultMap.put("message", "success");
+        resultMap.put("status", 200);
+        resultMap.put("data", userVO);
         return resultMap;
     }
 }
