a2d65642e0bdd6ce729e114d517d95e92eb4f2aa
kapcb <eircccallroot@yeah.net>
2021-04-24 13:08:08 +0800

    SpringBoot-Security-Auth2.0

---
+++ SpringBoot-Security-Auth2/src/main/java/com/kapcb/ccc/Auth2Application.java
@@ -0,0 +1,24 @@
+package com.kapcb.ccc;
+
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
+/**
+ * <a>Title: Auth2Application </a>
+ * <a>Author: kapcb <a>
+ * <a>Description: Auth 2.0 Application <a>
+ *
+ * @author kapcb
+ * @version 1.0.0
+ * @date 2021/4/24 - 13:06
+ */
+@SpringBootApplication
+public class Auth2Application {
+
+    public static void main(String[] args) {
+        new SpringApplicationBuilder()
+                .web(WebApplicationType.SERVLET)
+                .run(args);
+    }
+}
