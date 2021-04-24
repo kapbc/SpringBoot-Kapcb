93feb8bad9ab8ce54f4b1438ba3c84c3525db170
kapcb <eircccallroot@yeah.net>
2021-04-24 13:04:17 +0800

    SpringBoot-Job-XxlJob

---
+++ SpringBoot-Job-xxlJob/src/main/java/com/kapcb/ccc/XxlJobApplication.java
@@ -0,0 +1,26 @@
+package com.kapcb.ccc;
+
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
+/**
+ * <a>Title: XxlJobApplication </a>
+ * <a>Author: kapcb <a>
+ * <a>Description: XxlJob Application <a>
+ *
+ * https://www.cnblogs.com/ysocean/p/10541151.html
+ *
+ * @author kapcb
+ * @version 1.0.0
+ * @date 2021/4/24 - 13:01
+ */
+@SpringBootApplication
+public class XxlJobApplication {
+
+    public static void main(String[] args) {
+        new SpringApplicationBuilder()
+                .web(WebApplicationType.SERVLET)
+                .run(args);
+    }
+}
