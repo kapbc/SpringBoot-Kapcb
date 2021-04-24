2cadc550d0ce1886c3ad026fb801f36ecbcacf68
kapcb <eircccallroot@yeah.net>
2021-04-24 13:11:21 +0800

    SpringBoot-Network-Gateway

---
+++ SpringBoot-Network-Gateway/src/main/java/com/kapcb/ccc/GatewayApplication.java
@@ -0,0 +1,24 @@
+package com.kapcb.ccc;
+
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
+/**
+ * <a>Title: GatewayApplication </a>
+ * <a>Author: kapcb <a>
+ * <a>Description: Gateway Application <a>
+ *
+ * @author kapcb
+ * @version 1.0.0
+ * @date 2021/4/24 - 13:09
+ */
+@SpringBootApplication
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        new SpringApplicationBuilder()
+                .web(WebApplicationType.SERVLET)
+                .run(args);
+    }
+}
