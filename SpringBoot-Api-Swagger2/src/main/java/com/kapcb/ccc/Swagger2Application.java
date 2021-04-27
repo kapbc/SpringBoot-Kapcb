25516d74bf29b5ae5658dd1b5b0f12fde3f419f4
kapcb <eircccallroot@yeah.net>
2021-04-27 22:32:18 +0800

    add-new-idea-license

--- SpringBoot-Api-Swagger2/src/main/java/com/kapcb/ccc/Swagger2Application.java
+++ SpringBoot-Api-Swagger2/src/main/java/com/kapcb/ccc/Swagger2Application.java
@@ -1,7 +1,10 @@
 package com.kapcb.ccc;
 
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 /**
  * <a>Title: Swagger2Application </a>
@@ -15,7 +18,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 @SpringBootApplication
 public class Swagger2Application {
+
     public static void main(String[] args) {
-        SpringApplication.run(Swagger2Application.class, args);
+
+        new SpringApplicationBuilder()
+                .sources(Swagger2Application.class)
+                .bannerMode(Banner.Mode.OFF)
+                .web(WebApplicationType.SERVLET)
+                .run(args);
     }
 }
