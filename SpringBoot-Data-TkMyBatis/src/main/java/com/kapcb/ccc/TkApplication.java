923fe1c457bcdd08c7b6bb4cb96849d6241716dd
kapcb <eircccallroot@yeah.net>
2021-05-05 15:42:55 +0800

    SpringBoot-Data-TK

--- SpringBoot-Data-TkMyBatis/src/main/java/com/kapcb/ccc/TkApplication.java
+++ SpringBoot-Data-TkMyBatis/src/main/java/com/kapcb/ccc/TkApplication.java
@@ -1,7 +1,9 @@
 package com.kapcb.ccc;
 
-import org.springframework.boot.SpringApplication;
+import org.springframework.boot.Banner;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 /**
  * <a>Title: TkApplication </a>
