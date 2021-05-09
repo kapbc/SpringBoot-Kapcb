871e9d0a1fc040747386eaf369dbc479bcc209da
kapcb <eircccallroot@yeah.net>
2021-05-09 18:36:19 +0800

    SprngBoot-Cache-Redis

---
+++ SpringBoot-Cache-Redis/src/main/java/com/kapcb/ccc/common/Configuration/RedisConfiguration.java
@@ -0,0 +1,20 @@
+package com.kapcb.ccc.common.Configuration;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * <a>Title: SpringBoot-Kapcb </a>
+ * <a>Author: Kapcb <a>
+ * <a>Description: Redis Configuration <a>
+ *
+ * @author Kapcb
+ * @version 1.0.0
+ * @date 2021/5/9 18:28
+ */
+@Slf4j
+@Configuration
+public class RedisConfiguration {
+
+    
+}
