a1298e7573540bdd1368ec68ac65a041331e5dae
kapcb <eircccallroot@yeah.net>
2021-05-18 22:13:21 +0800

    Spring Boot Batch Sync Select

---
+++ SpringBoot-Async-AsyncSelecct/src/main/java/com/kapcb/ccc/common/config/TaskConfiguration.java
@@ -0,0 +1,16 @@
+package com.kapcb.ccc.common.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * <a>Title: SpringBoot-Kapcb </a>
+ * <a>Author: Kapcb <a>
+ * <a>Description:  <a>
+ *
+ * @author Kapcb
+ * @version 1.0.0
+ * @date 2021/5/18 22:11
+ */
+@Configuration
+public class TaskConfiguration {
+}
---
+++ SpringBoot-Async-AsyncSelecct/src/main/java/com/kapcb/ccc/service/impl/UserMainServiceImpl.java
@@ -0,0 +1,17 @@
+package com.kapcb.ccc.service.impl;
+
+import com.kapcb.ccc.service.UserMainService;
+import org.springframework.stereotype.Service;
+
+/**
+ * <a>Title: SpringBoot-Kapcb </a>
+ * <a>Author: Kapcb <a>
+ * <a>Description:  <a>
+ *
+ * @author Kapcb
+ * @version 1.0.0
+ * @date 2021/5/18 22:12
+ */
+@Service
+public class UserMainServiceImpl implements UserMainService {
+}
