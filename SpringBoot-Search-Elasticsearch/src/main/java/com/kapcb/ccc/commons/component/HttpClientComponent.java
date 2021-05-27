7c4678318458fb4e3db4be8af60307044de7073d
kapcb <eircccallroot@yeah.net>
2021-05-27 22:34:05 +0800

    SpringBoot-Search-Elasticsearch

--- SpringBoot-Search-Elasticsearch/src/main/java/com/kapcb/ccc/commons/component/HttpClientComponent.java
+++ SpringBoot-Search-Elasticsearch/src/main/java/com/kapcb/ccc/commons/component/HttpClientComponent.java
@@ -1,8 +1,13 @@
 package com.kapcb.ccc.commons.component;
 
 import com.kapcb.ccc.commons.properties.ElasticsearchProperties;
+import org.apache.http.Header;
 import org.apache.http.HttpHost;
+import org.apache.http.message.BasicHeader;
+import org.elasticsearch.client.Node;
+import org.elasticsearch.client.NodeSelector;
 import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestClientBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
@@ -22,13 +27,35 @@ import java.io.IOException;
 @Component
 //@ConditionalOnProperty()
 public class HttpClientComponent {
+    
     private static final Logger log = LoggerFactory.getLogger(HttpClientComponent.class);
 
     private RestClient restClient;
 
     @PostConstruct
     public void init(ElasticsearchProperties elasticsearchProperties) {
-        this.restClient = RestClient.builder(new HttpHost(elasticsearchProperties.getHost(), elasticsearchProperties.getPort(), elasticsearchProperties.getScheme())).build();
+        RestClientBuilder builder = RestClient.builder(new HttpHost(elasticsearchProperties.getHost(), elasticsearchProperties.getPort(), elasticsearchProperties.getScheme()));
+
+        // 请求头配置方法, 设置每个请求头默认需要发送的请求头配置
+        Header[] headers = {new BasicHeader("header", "kapcb")};
+        builder.setDefaultHeaders(headers);
+
+        // 配置监听器, 该监听器在每次节点初始化失败时都会收到通知, 在启用嗅探器时在内部使用
+        builder.setFailureListener(new RestClient.FailureListener(){
+            @Override
+            public void onFailure(Node node) {
+                super.onFailure(node);
+            }
+        });
+
+        // 配置节点选择器
+        builder.setNodeSelector(NodeSelector.SKIP_DEDICATED_MASTERS);
+
+        // 设置超时时间
+        builder.setRequestConfigCallback(requestConfigBuilder -> requestConfigBuilder.setSocketTimeout(10000));
+
+        this.restClient = builder.build();
+
         log.info("elasticsearch rest client init success!");
     }
 
