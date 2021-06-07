8a4b532db90e34bcd10a13aa0d5d5b385782cedb
kapcb <eircccallroot@yeah.net>
2021-06-07 19:39:15 +0800

    Create SystemController.java

---
+++ SpringBoot-IM-Netty-WebSocket/src/main/java/com/kapcb/ccc/controller/SystemController.java
@@ -0,0 +1,23 @@
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * <a>Title: SystemController </a>
+ * <a>Author: Mike Chen <a>
+ * <a>Description:  <a>
+ *
+ * @author Mike Chen
+ * @date 2021/6/3-17:58
+ */
+@Controller
+@RequestMapping(value = "web")
+public class SystemController {
+
+    @GetMapping(value = "/socket")
+    public String index() {
+        return "index";
+    }
+}
