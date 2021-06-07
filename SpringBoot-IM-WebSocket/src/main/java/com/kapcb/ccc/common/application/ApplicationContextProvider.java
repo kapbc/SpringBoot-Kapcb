bf0887dcf4468a15bc62b1a20cfd81207483277f
kapcb <eircccallroot@yeah.net>
2021-06-07 19:43:32 +0800

    Create ApplicationContextProvider.java

---
+++ SpringBoot-IM-WebSocket/src/main/java/com/kapcb/ccc/common/application/ApplicationContextProvider.java
@@ -0,0 +1,44 @@
+
+
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.stereotype.Component;
+
+/**
+ * <a>Title: ApplicationContextProvider </a>
+ * <a>Author: Mike Chen <a>
+ * <a>Description: Application Context Provider <a>
+ *
+ * @author Mike Chen
+ * @date 2021/6/2-18:39
+ */
+@Component
+public class ApplicationContextProvider implements ApplicationContextAware {
+
+    /**
+     * 上下文对象实例
+     */
+    private static ApplicationContext applicationContext;
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        ApplicationContextProvider.applicationContext = applicationContext;
+    }
+
+    public static ApplicationContext getApplicationContext() {
+        return applicationContext;
+    }
+
+    public Object getBean(String name) {
+        return getApplicationContext().getBean(name);
+    }
+
+    public <T> T getBean(Class<T> clazz) {
+        return getApplicationContext().getBean(clazz);
+    }
+
+    public <T> T getBean(String name, Class<T> clazz) {
+        return getApplicationContext().getBean(name, clazz);
+    }
+}
