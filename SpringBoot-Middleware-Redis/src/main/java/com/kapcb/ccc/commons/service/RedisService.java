9c01ca84f43c8aa34ae096401509097355322425
kapcb <eircccallroot@yeah.net>
2021-06-08 21:52:02 +0800

    project-refactory-refi

---
+++ SpringBoot-Middleware-Redis/src/main/java/com/kapcb/ccc/commons/service/RedisService.java
@@ -0,0 +1,17 @@
+package com.kapcb.ccc.commons.service;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * <a>Title: SpringBoot-Kapcb </a>
+ * <a>Author: Kapcb <a>
+ * <a>Description:  <a>
+ *
+ * @author Kapcb
+ * @version 1.0.0
+ * @date 2021/6/8 21:51
+ */
+@Slf4j
+@SuppressWarnings("all")
+public class RedisService {
+}
