cc2b462afa1b4446e344bd40ccf9b10469ff4ada
kapcb <eircccallroot@yeah.net>
2021-07-13 22:31:04 +0800

    project-refacotry

---
+++ SpringBoot-Data-MyBatisPlus/src/main/java/com/kapcb/ccc/mybatis/plus/MyBatisPlusApplication.java
@@ -0,0 +1,13 @@
+package com.kapcb.ccc.mybatis.plus;
+
+/**
+ * <a>Title: MyBatisPlusApplication </a>
+ * <a>Author: Kapcb <a>
+ * <a>Description:  <a>
+ *
+ * @author Kapcb
+ * @version 1.0.0
+ * @date 2021/7/13 22:30
+ */
+public class MyBatisPlusApplication {
+}
