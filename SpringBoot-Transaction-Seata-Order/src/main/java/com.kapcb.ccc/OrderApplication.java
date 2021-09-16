30c2c862d8dbf6951178874f797e2819889a056f
eircccallroot@yeah.net <eircccallroot@yeah.net>
2021-09-16 23:05:38 +0800

    seata

---
+++ SpringBoot-Transaction-Seata-Order/src/main/java/com.kapcb.ccc/OrderApplication.java
@@ -0,0 +1,13 @@
+package com.kapcb.ccc;
+
+/**
+ * <a>Title: OrderApplication </a>
+ * <a>Author: Kapcb <a>
+ * <a>Description:  <a>
+ *
+ * @author Kapcb
+ * @version 1.0.0
+ * @date 2021/9/16 22:39
+ */
+public class OrderApplication {
+}
